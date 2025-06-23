f043edb5187bcd6864fbf4d1205bc3005ac828e9
Goodness-MArcel <anyanwugoodness83@yahoo.com>
2025-06-23 22:12:51 +0100

    Update vite.config.js

--- forntend/vite.config.js
+++ forntend/vite.config.js
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://joblisting-backend-m2wa.onrender.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api') // Keep '/api' prefix
-      },
-      // '/socket.io': {
-      //   target: 'http://localhost:3000',
-      //   ws: true
-      // }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://joblisting-backend-m2wa.onrender.com',
+  //       changeOrigin: true,
+  //       secure: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '/api') // Keep '/api' prefix
+  //     },
+  //     // '/socket.io': {
+  //     //   target: 'http://localhost:3000',
+  //     //   ws: true
+  //     // }
+  //   }
+  // }
 });
